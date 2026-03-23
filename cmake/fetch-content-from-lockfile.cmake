b37366a9e8d6fb8334df8e83fbae4a8b173ed410
MarkusB <>
2026-03-24 00:56:12 +0100

    Normalize indentation in `fetch-content-from-lockfile.cmake` for consistent formatting.

--- cmake/fetch-content-from-lockfile.cmake
+++ cmake/fetch-content-from-lockfile.cmake
@@ -20,149 +20,157 @@ if(IS_ABSOLUTE "${MB_FETCHCONTENT_LOCKFILE}")
 endif()
 
 if(EXISTS "${lockfile_candidate}")
-message(TRACE "MB_FETCHCONTENT_LOCKFILE=\"${MB_FETCHCONTENT_LOCKFILE}\"")
-file(
-    REAL_PATH
-    "${MB_FETCHCONTENT_LOCKFILE}"
-    consumer_fetchcontent_lockfile
-    BASE_DIRECTORY "${consumer_project_dir}"
-    EXPAND_TILDE
-)
-message(
-    DEBUG
-    "Using FetchContent lockfile: \"${consumer_fetchcontent_lockfile}\""
-)
-
-# Force CMake to reconfigure the project if the lockfile changes
-set_property(
-    DIRECTORY "${consumer_project_dir}"
-    APPEND
-    PROPERTY CMAKE_CONFIGURE_DEPENDS "${consumer_fetchcontent_lockfile}"
-)
-
-# For more on the protocol for this function, see:
-# https://cmake.org/cmake/help/latest/command/cmake_language.html#provider-commands
-function(mb_fetchcontent_provide_dependency method package_name)
-    # Read the lockfile
-    file(READ "${consumer_fetchcontent_lockfile}" root_obj)
-
-    # Get the "dependencies" field and store it in dependencies_obj
-    string(
-        JSON
-        dependencies_obj
-        ERROR_VARIABLE error
-        GET "${root_obj}"
-        "dependencies"
+    message(TRACE "MB_FETCHCONTENT_LOCKFILE=\"${MB_FETCHCONTENT_LOCKFILE}\"")
+    file(
+        REAL_PATH
+        "${MB_FETCHCONTENT_LOCKFILE}"
+        consumer_fetchcontent_lockfile
+        BASE_DIRECTORY "${consumer_project_dir}"
+        EXPAND_TILDE
     )
-    if(error)
-        message(FATAL_ERROR "${consumer_fetchcontent_lockfile}: ${error}")
-    endif()
-
-    # Get the length of the libraries array and store it in dependencies_obj
-    string(
-        JSON
-        num_dependencies
-        ERROR_VARIABLE error
-        LENGTH "${dependencies_obj}"
+    message(
+        DEBUG
+        "Using FetchContent lockfile: \"${consumer_fetchcontent_lockfile}\""
     )
-    if(error)
-        message(FATAL_ERROR "${consumer_fetchcontent_lockfile}: ${error}")
-    endif()
 
-    if(num_dependencies EQUAL 0)
-        return()
-    endif()
+    # Force CMake to reconfigure the project if the lockfile changes
+    set_property(
+        DIRECTORY "${consumer_project_dir}"
+        APPEND
+        PROPERTY CMAKE_CONFIGURE_DEPENDS "${consumer_fetchcontent_lockfile}"
+    )
 
-    # Loop over each dependency object
-    math(EXPR max_index "${num_dependencies} - 1")
-    foreach(index RANGE "${max_index}")
-        set(error_prefix
-            "${consumer_fetchcontent_lockfile}, dependency ${index}"
-        )
+    # For more on the protocol for this function, see:
+    # https://cmake.org/cmake/help/latest/command/cmake_language.html#provider-commands
+    function(mb_fetchcontent_provide_dependency method package_name)
+        # Read the lockfile
+        file(READ "${consumer_fetchcontent_lockfile}" root_obj)
 
-        # Get the dependency object at index
-        # and store it in dep_obj
+        # Get the "dependencies" field and store it in dependencies_obj
         string(
             JSON
-            dep_obj
+            dependencies_obj
             ERROR_VARIABLE error
-            GET "${dependencies_obj}"
-            "${index}"
+            GET "${root_obj}"
+            "dependencies"
         )
         if(error)
-            message(FATAL_ERROR "${error_prefix}: ${error}")
+            message(FATAL_ERROR "${consumer_fetchcontent_lockfile}: ${error}")
         endif()
 
-        # Get the "name" field and store it in name
-        string(JSON name ERROR_VARIABLE error GET "${dep_obj}" "name")
-        if(error)
-            message(FATAL_ERROR "${error_prefix}: ${error}")
-        endif()
-
-        # Get the "package_name" field and store it in pkg_name
+        # Get the length of the libraries array and store it in dependencies_obj
         string(
             JSON
-            pkg_name
+            num_dependencies
             ERROR_VARIABLE error
-            GET "${dep_obj}"
-            "package_name"
+            LENGTH "${dependencies_obj}"
         )
         if(error)
-            message(FATAL_ERROR "${error_prefix}: ${error}")
+            message(FATAL_ERROR "${consumer_fetchcontent_lockfile}: ${error}")
         endif()
 
-        # Get the "git_repository" field and store it in repo
-        string(JSON repo ERROR_VARIABLE error GET "${dep_obj}" "git_repository")
-        if(error)
-            message(FATAL_ERROR "${error_prefix}: ${error}")
+        if(num_dependencies EQUAL 0)
+            return()
         endif()
 
-        # Get the "git_tag" field and store it in tag
-        string(JSON tag ERROR_VARIABLE error GET "${dep_obj}" "git_tag")
-        if(error)
-            message(FATAL_ERROR "${error_prefix}: ${error}")
-        endif()
+        # Loop over each dependency object
+        math(EXPR max_index "${num_dependencies} - 1")
+        foreach(index RANGE "${max_index}")
+            set(error_prefix
+                "${consumer_fetchcontent_lockfile}, dependency ${index}"
+            )
 
-        if(method STREQUAL "FIND_PACKAGE")
-            if(package_name STREQUAL pkg_name)
-                string(
-                    APPEND
-                    debug
-                    "Redirecting find_package calls for ${pkg_name} "
-                    "to FetchContent logic.\n"
-                )
-                string(
-                    APPEND
-                    debug
-                    "Fetching ${repo} at "
-                    "${tag} according to ${consumer_fetchcontent_lockfile}."
-                )
-                message(DEBUG "${debug}")
-                FetchContent_Declare(
-                    "${name}"
-                    GIT_REPOSITORY "${repo}"
-                    GIT_TAG "${tag}"
-                    EXCLUDE_FROM_ALL
-                )
-                set(INSTALL_GTEST OFF) # Disable GoogleTest installation
-                FetchContent_MakeAvailable("${name}")
-
-                # Important! <PackageName>_FOUND tells CMake that `find_package` is
-                # not needed for this package anymore
-                set("${pkg_name}_FOUND" TRUE PARENT_SCOPE)
+            # Get the dependency object at index
+            # and store it in dep_obj
+            string(
+                JSON
+                dep_obj
+                ERROR_VARIABLE error
+                GET "${dependencies_obj}"
+                "${index}"
+            )
+            if(error)
+                message(FATAL_ERROR "${error_prefix}: ${error}")
             endif()
-        endif()
-    endforeach()
-endfunction()
 
-cmake_language(
-    SET_DEPENDENCY_PROVIDER mb_fetchcontent_provide_dependency
-    SUPPORTED_METHODS FIND_PACKAGE
-)
+            # Get the "name" field and store it in name
+            string(JSON name ERROR_VARIABLE error GET "${dep_obj}" "name")
+            if(error)
+                message(FATAL_ERROR "${error_prefix}: ${error}")
+            endif()
 
-# Add this dir to the module path so that `find_package(...)` works
-list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
+            # Get the "package_name" field and store it in pkg_name
+            string(
+                JSON
+                pkg_name
+                ERROR_VARIABLE error
+                GET "${dep_obj}"
+                "package_name"
+            )
+            if(error)
+                message(FATAL_ERROR "${error_prefix}: ${error}")
+            endif()
 
+            # Get the "git_repository" field and store it in repo
+            string(
+                JSON
+                repo
+                ERROR_VARIABLE error
+                GET "${dep_obj}"
+                "git_repository"
+            )
+            if(error)
+                message(FATAL_ERROR "${error_prefix}: ${error}")
+            endif()
+
+            # Get the "git_tag" field and store it in tag
+            string(JSON tag ERROR_VARIABLE error GET "${dep_obj}" "git_tag")
+            if(error)
+                message(FATAL_ERROR "${error_prefix}: ${error}")
+            endif()
+
+            if(method STREQUAL "FIND_PACKAGE")
+                if(package_name STREQUAL pkg_name)
+                    string(
+                        APPEND
+                        debug
+                        "Redirecting find_package calls for ${pkg_name} "
+                        "to FetchContent logic.\n"
+                    )
+                    string(
+                        APPEND
+                        debug
+                        "Fetching ${repo} at "
+                        "${tag} according to ${consumer_fetchcontent_lockfile}."
+                    )
+                    message(DEBUG "${debug}")
+                    FetchContent_Declare(
+                        "${name}"
+                        GIT_REPOSITORY "${repo}"
+                        GIT_TAG "${tag}"
+                        EXCLUDE_FROM_ALL
+                    )
+                    set(INSTALL_GTEST OFF) # Disable GoogleTest installation
+                    FetchContent_MakeAvailable("${name}")
+
+                    # Important! <PackageName>_FOUND tells CMake that `find_package` is
+                    # not needed for this package anymore
+                    set("${pkg_name}_FOUND" TRUE PARENT_SCOPE)
+                endif()
+            endif()
+        endforeach()
+    endfunction()
+
+    cmake_language(
+        SET_DEPENDENCY_PROVIDER mb_fetchcontent_provide_dependency
+        SUPPORTED_METHODS FIND_PACKAGE
+    )
+
+    # Add this dir to the module path so that `find_package(...)` works
+    list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
 else()
-    message(STATUS "FetchContent lockfile not found (${lockfile_candidate}), skipping lockfile dependency provider")
+    message(
+        STATUS
+        "FetchContent lockfile not found (${lockfile_candidate}), skipping lockfile dependency provider"
+    )
 endif()
